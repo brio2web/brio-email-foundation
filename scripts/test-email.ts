ba55c4bbd80332bbd9f04ead8eae4d4c0e7c0cfe
brio2web <brio2web@gmail.com>
2026-08-27 15:13:00 -0400

    Create test-email.ts

---
+++ scripts/test-email.ts
@@ -0,0 +1,18 @@
+import 'dotenv/config';
+
+import { sendEmail } from '../src/index';
+
+const result = await sendEmail({
+  to: 'brio2web@gmail.com',
+  subject: 'Brio Email Foundation Test',
+  template: 'welcome',
+  data: {
+    name: 'Brandon',
+  },
+});
+
+console.log(result);
+
+if (!result.success) {
+  process.exit(1);
+}
